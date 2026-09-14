2f951b5c0c84776fd356f7337f0d353d468b3796
JONATHAN9006 <43485609+JONATHAN9006@users.noreply.github.com>
2026-09-13 22:45:00 -0500

    Actualizacion completa

--- public/config.js
+++ public/config.js
@@ -10,7 +10,7 @@
 //   el Analista IA se muestra igual, pero avisa que no está configurado.
 
 window.APP_CONFIG = {
-  backendUrl: "https://script.google.com/macros/s/AKfycbxxSpQQWb4W_7PCFGSWQPwjG3Vi3oPWXan80VI3_V4PCox-bZRhX-jjgFsqrKpazfUe8g/exec",
+  backendUrl: "https://script.google.com/macros/s/AKfycbw5YKQVuG9z0nvgCNqqSvxm9x_7EKqX2XAyoTcXY215CZEbj8b35sgARZozhAa6IegG/exec",
   updatePassword: "Mepoy-2026",
   agenteIAUrl: "",
 };
